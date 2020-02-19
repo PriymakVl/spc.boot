b4446caf9b53efc0ffaba0b268e27604362a6b20
unknown <C:\Users\\AppData\Roaming\The Bat!\Mail>
2020-02-19 16:01:17 +0200

    19-02-20-work

--- modules/product/views/product/index/main.php
+++ modules/product/views/product/index/main.php
@@ -3,6 +3,7 @@ use yii\helpers\Html;
 
 $this->registerCssFile('@web/css/content/product.css');
 $this->registerCssFile('@web/css/content/product_sidebar.css');
+$this->registerCssFile('@web/css/content/product_description.css');
 ?>
 
 <?=$this->render('breadcrumbs', ['cat' => $cat])?>
@@ -25,9 +26,9 @@ $this->registerCssFile('@web/css/content/product_sidebar.css');
 			</div> <!-- /.product__content -->
 		</div> <!-- /.product__inner -->
 
-		<?= $this->render('product_description') ?>
-
 	</div> <!-- /.product -->
 
 	<?=$this->render('sidebar')?>
 </div>
+
+<?= $this->render('product_description') ?>
--- modules/product/views/product/index/product_description.php
+++ modules/product/views/product/index/product_description.php
@@ -1,15 +1,21 @@
 <div class="product__description">
-	<h2>Технические характеристики</h2>
-	<tabel class="product__specification">
-		<tr>
-			<th></th>
-			<th></th>
-			<th></th>
-		</tr>
-		<tr>
-			<th></th>
-			<th></th>
-			<th></th>
-		</tr>
-	</tabel>
+	<div class="tabs">
+		<div class="tab">
+			<input type="radio" id="tab1" name="tab-group" checked>
+			<label for="tab1" class="tab-title">Технические характеристики</label> 
+			<section class="tab-content">
+				<?//= $this->render('product_specification') ?>
+			</section>
+		</div> 
+		<div class="tab">
+			<input type="radio" id="tab2" name="tab-group">
+			<label for="tab2" class="tab-title">Габаритные размеры</label> 
+			<section class="tab-content">Два</section>
+		</div>
+		<div class="tab">
+			<input type="radio" id="tab3" name="tab-group">
+			<label for="tab3" class="tab-title">Описание</label> 
+			<section class="tab-content">Три</section> 
+		</div> 
+	</div>
 </div>
